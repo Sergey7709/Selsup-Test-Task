a5bbb950beffb57955a26e84d90b2666ee51ca88
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-13 13:01:22 +0300

    refactor: delete unused constants and add variables params, model

--- src/pages/dashboard/constants.dashboard.ts
+++ src/pages/dashboard/constants.dashboard.ts
@@ -1,5 +1,36 @@
-export const magicNumber = 8005
-export const defaultPage = 1
-export const defaultLimit = 50
-export const defaultOffset = 0
-export const defaultValueParams = { limit: defaultLimit, offset: defaultOffset }
+import { Model, Param } from '@/pages/dashboard/types.dashboard'
+
+export const params: Param[] = [
+  {
+    id: 1,
+    name: 'Назначение',
+    type: 'string',
+  },
+  {
+    id: 2,
+    name: 'Длина',
+    type: 'string',
+  },
+  {
+    id: 3,
+    name: 'Ширина',
+    type: 'string',
+  },
+]
+
+export const model: Model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: 'повседневное',
+    },
+    {
+      paramId: 2,
+      value: 'макси',
+    },
+    {
+      paramId: 3,
+      value: 'мини',
+    },
+  ],
+}
