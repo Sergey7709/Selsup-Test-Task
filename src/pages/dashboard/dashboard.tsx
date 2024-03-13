5f8eb78b7b3fc48ca0945e3d7aecb9d543fdeca5
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-13 22:19:53 +0300

    refactor: edit JSX

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -3,8 +3,8 @@ import { model, params } from '@/pages/dashboard/constants.dashboard'
 
 export const Dashboard = () => {
   return (
-    <div>
+    <>
       <EditorBlock model={model} params={params} />
-    </div>
+    </>
   )
 }
