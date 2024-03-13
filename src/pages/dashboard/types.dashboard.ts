63a3561d4c1da0cbf874a923c4e89c2a00539e80
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-13 21:43:52 +0300

    refactor: edit types ParamType. Edit interface  ParamValue. Add type FieldEditorProps.

--- src/pages/dashboard/types.dashboard.ts
+++ src/pages/dashboard/types.dashboard.ts
@@ -1,17 +1,27 @@
-type ParamType = 'number' | 'select' | 'string'
-type OptionsType = number[] | string[]
+type ParamType = 'list' | 'number' | 'string'
 export interface Param {
   id: number
   name: string
-  options?: OptionsType
   type: ParamType
 }
 
 export interface ParamValue {
   paramId: number
-  value: string
+  value: (number | string)[] | number | string
 }
 
 export interface Model {
   paramValues: ParamValue[]
 }
+
+export type FieldEditorProps = {
+  handleInputChange: (id: number, index: number, newValue: number | string) => void
+  label: string
+  param: Param
+  paramValues: ParamValue[]
+  typeField: 'number' | 'string'
+}
+
+export type SelectorEditorProps = {
+  model: Model
+} & Omit<FieldEditorProps, 'label' | 'typeField'>
