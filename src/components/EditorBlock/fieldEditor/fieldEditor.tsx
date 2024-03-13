12df0eb42ff55ecd9238144e2626f6048c5d0753
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-13 13:40:50 +0300

    refactor: rename component StringFieldEditor on fieldEditor

--- src/components/EditorBlock/fieldEditor/fieldEditor.tsx
+++ src/components/EditorBlock/fieldEditor/fieldEditor.tsx
@@ -3,24 +3,26 @@ import { FC } from 'react'
 import { TextField } from '@/components/ui/text-field'
 import { Param, ParamValue } from '@/pages/dashboard/types.dashboard'
 
-type StringFieldEditorProps = {
+type fieldEditorProps = {
   handleInputChange: (id: number, value: string) => void
   label: string
   param: Param
   paramValues: ParamValue[]
+  typeField: 'number' | 'string'
 }
-export const StringFieldEditor: FC<StringFieldEditorProps> = ({
+export const FieldEditor: FC<fieldEditorProps> = ({
   handleInputChange,
   label,
   param,
   paramValues,
+  typeField,
 }) => {
   return (
     <div>
       <TextField
         label={label}
         onChange={e => handleInputChange(param.id, e.target.value)}
-        type={'text'}
+        type={typeField}
         value={paramValues.find(paramValue => paramValue.paramId === param.id)?.value || ''}
       />
     </div>
