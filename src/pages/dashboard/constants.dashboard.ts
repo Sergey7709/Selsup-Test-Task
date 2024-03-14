6e7a853f7d0e0f0a82aefb505200a40d248a5d5d
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-14 10:43:38 +0300

    feat: Add variables for number, string and list values.

--- src/pages/dashboard/constants.dashboard.ts
+++ src/pages/dashboard/constants.dashboard.ts
@@ -1,6 +1,6 @@
 import { Model, Param } from '@/pages/dashboard/types.dashboard'
 
-export const params: Param[] = [
+export const paramsString: Param[] = [
   {
     id: 1,
     name: 'Назначение',
@@ -18,7 +18,7 @@ export const params: Param[] = [
   },
 ]
 
-export const model: Model = {
+export const modelString: Model = {
   paramValues: [
     {
       paramId: 1,
@@ -31,6 +31,60 @@ export const model: Model = {
     {
       paramId: 3,
       value: 'мини',
+    },
+  ],
+}
+
+
+export const paramsNumber: Param[] = [
+  {
+    id: 1,
+    name: 'Высота',
+    type: 'number',
+  },
+  {
+    id: 2,
+    name: 'Длина',
+    type: 'number',
+  },
+  {
+    id: 3,
+    name: 'Ширина',
+    type: 'number',
+  },
+]
+
+export const modelNumber: Model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: 100,
+    },
+    {
+      paramId: 2,
+      value: 1000,
+    },
+    {
+      paramId: 3,
+      value: 500,
+    },
+  ],
+}
+
+
+export const paramsStringArray: Param[] = [
+  {
+    id: 1,
+    name: 'Цвет',
+    type: 'list',
+  },
+]
+
+export const modelStringArray: Model = {
+  paramValues: [
+    {
+      paramId: 1,
+      value: ['Красный', 'Зеленый', 'Синий'],
     },
   ],
 }
