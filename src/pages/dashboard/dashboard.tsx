753933e17b63ceea189989365fe349c1b3a52969
Sergey Koshelev <shivacharya2019@gmail.com>
2024-03-14 10:48:33 +0300

    feat: add in JSX components Typography and  EditorBlock for all variant data.

--- src/pages/dashboard/dashboard.tsx
+++ src/pages/dashboard/dashboard.tsx
@@ -1,10 +1,37 @@
 import { EditorBlock } from '@/components/EditorBlock'
-import { model, params } from '@/pages/dashboard/constants.dashboard'
+import { Typography } from '@/components/ui/typography'
+import {
+  modelNumber,
+  modelString,
+  modelStringArray,
+  paramsNumber,
+  paramsString,
+  paramsStringArray,
+} from '@/pages/dashboard/constants.dashboard'
+
+import s from './dashboard.module.scss'
 
 export const Dashboard = () => {
   return (
-    <>
-      <EditorBlock model={model} params={params} />
-    </>
+    <div className={s.wrapper_panel}>
+      <div className={s.wrapper_header_typography}>
+        <Typography className={s.header_typography} variant={'h3'}>
+          В решении реализована функциональность передачи не только текстовых данных, но и числовых,
+          а также списков значений, тип которых определяется структурой params и model.
+        </Typography>
+      </div>
+      <Typography className={s.title_typography} variant={'subtitle1'}>
+        Пример текстовых данных
+      </Typography>
+      <EditorBlock model={modelString} params={paramsString} />
+      <Typography className={s.title_typography} variant={'subtitle1'}>
+        Пример списка текстовых данных
+      </Typography>
+      <EditorBlock model={modelStringArray} params={paramsStringArray} />
+      <Typography className={s.title_typography} variant={'subtitle1'}>
+        Пример списка числовых данных
+      </Typography>
+      <EditorBlock model={modelNumber} params={paramsNumber} />
+    </div>
   )
 }
